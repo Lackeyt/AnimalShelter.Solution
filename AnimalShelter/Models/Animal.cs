95dd9d19b105aa498a57012f99ec6c77effb34ea
Tyson Lackey <lackeyt90@gmail.com>
2020-07-28 10:52:57 -0700

    add gender enum and configure database
    Co-authored-by: JohnNils Olson <johnnils@gmail.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -8,9 +8,13 @@ namespace AnimalShelter.Models
     public int AnimalId { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
-    public string Gender { get; set; }
+    public Gender AnimalGender { get; set; }
     public DateTime DateOfAdmittance { get; set; }
     public string Breed { get; set; }
-
+    public enum Gender
+    {
+    Male,
+    Female    
+    }
   }
 }
